6e7931344473088929433c7f25bd3a1e7a935d5f
Chris Tomei <christomei@gmail.com>
2020-01-19 21:23:42 -0500

    Readme updated and more exercising

---
+++ ex3.py
@@ -0,0 +1,18 @@
+print "I will not count my chickens:"
+
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7 ?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "Is it greater?", 100.0 % 75.0 / 5.0 < 2.0 * 2.0
+
+print "Is it greater or equal?", 129.0 % 75.0 / 4.0 >= 2.0 * 3.0
+
+print "Is it less than or equal to?", 150.0 % 75.0 <= 1.0 * 1.0
