3505d08f59e26da6da86c69cbf8e9603dcc3ff68
Chris Tomei <christomei@gmail.com>
2020-01-19 20:54:48 -0500

    First 2 exercises.

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment. Octothorpe is my new preferred name for that symbol. 
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to disable or comment out a piece of code:
+# print "This will not run."
+
+print "This will run."
